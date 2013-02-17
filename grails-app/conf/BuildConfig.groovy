deabe5f29062d71cb8dae23f9f20c44cfe517799
Paul Lee <mailpaul@gmail.com>
2013-02-17 12:13:34 -0500

    Added controller to load dummy survey data

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,5 +59,8 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.2.1"
 
         compile ':cache:1.0.1'
+        compile ':fixtures:1.2'
+        compile ':build-test-data:2.0.4'
+        test ':build-test-data:2.0.4'
     }
 }
