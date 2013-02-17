bd26856285d4ed3e388fbd6bfa0729fc4cefc505
Paul Lee <plee@plee.local>
2013-02-17 03:48:14 -0500

    adding url mappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -11,5 +11,10 @@ class UrlMappings {
 		"/demo"(controller:"main", action:"demo")
 
 		"500"(view:'/error')
+		
+		"/survey/$surveyId/student/$userId" (controller: "survey", action: "index")
+		"/survey/$surveyId/student/$userId/save" (controller: "survey", action: "save")
+		"/survey/$surveyId/answers" (controller: "survey", action: "allAnswers")
+		"/survey/$surveyId/student/$userId/answers" (controller: "survey", action: "studentAnswers")
 	}
 }
--- grails-app/controllers/classcartography/PaulController.groovy
+++ grails-app/controllers/classcartography/PaulController.groovy
@@ -5,43 +5,16 @@ import groovy.xml.MarkupBuilder
 class PaulController {
 
     def index() {
-        withHttp(uri: "http://www.google.com") {
-           def html = get(path : '/search', query : [q:'Groovy'])
-           assert html.HEAD.size() == 1
-           assert html.BODY.size() == 1
-        }
-        render html
-    }
 
-    def insert() {
-
-        def q1 = new Question(question: "How much did you learn today?", responseType:"NUMBER");
-        q.save()
-        def q1get = Question.get(1)
-        asset 1 == q1get.id
-        def q2 = new Question(question: "What was your favorite part of class?", responseType:"TEXT");
-        q2.save()
-        def q3 = new Question(question: "What was your least favorite part of class?", responseType:"TEXT");
-        q3.save()
-        def q4 = new Question(question: "Describe one new thing you learned today.", responseType:"TEXT");
-        q4.save()
+      def data
+      withRest(id: "DEMO_KEY", uri: "http://twitter.com/statuses/") {
+        auth.basic 'DEMO_KEY', ''
+        def response = get(path: '/v1.1/schools')
+        data = response.data
+      }
+      render data
 
     }
+
     
-    def login() {
-        def writer = new StringWriter()
-        def builder = new MarkupBuilder(writer)
-        builder.html {
-            head {
-                title 'Log in'
-            }
-            body {
-                h1 'Hello'
-                form {
-                }
-            }
-        }
-        def html = writer.toString()
-        render html
-    }    
 }
